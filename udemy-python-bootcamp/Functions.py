8ccafc1d133fc8e4ca1a804d1f1606746a6b3626
Ahmed Alhassanat <a.hassanat89@gmail.com>
2026-08-03 00:19:03 +0200

    Remove blackjack function from Functions.py
    Removed the blackjack function and its documentation from Functions.py.

--- udemy-python-bootcamp/Functions.py
+++ udemy-python-bootcamp/Functions.py
@@ -1,25 +1,6 @@
 
 
 '''
-BLACKJACK: Given three integers between 1 and 11, 
-if their sum is less than or equal to 21, return their sum. 
-If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. 
-Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'¶
-'''
-
-
-def blackjack(a,b,c):
-    if sum ((a,b,c)) <= 21:
-        return sum ((a,b,c))
-    elif sum ((a,b,c)) > 21 and 11 in (a,b,c):
-        return sum((a,b,c)) -10
-    else:
-        return 'BUST!'
-
-
-**************************************************************************************************************************************************************************
-
-'''
 SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9  (every 6 will be followed by at least one 9). Return 0 for no numbers.
 '''
 def summer_69(arr):
