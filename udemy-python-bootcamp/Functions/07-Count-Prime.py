fc9764956cebabfd0d2193f4010081c011ef9a3d
Ahmed Alhassanat <a.hassanat89@gmail.com>
2026-08-03 00:33:12 +0200

    Add function to count prime numbers
    Implemented a function to count prime numbers up to a given number.

--- udemy-python-bootcamp/Functions/07-Count-Prime.py
+++ udemy-python-bootcamp/Functions/07-Count-Prime.py
@@ -1,1 +1,18 @@
-
+'''
+COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number
+'''
+def count_primes(num):
+    primes = [2]
+    x = 3
+    if num < 2:
+        return 0
+    while x <= num:
+        for y in primes:
+           if x%y == 0:
+               x += 1
+               break
+        else:
+            primes.append(x)
+            x+=1
+    print(primes)
+    return len(primes)
