1fa8803c814bd552d449e9bfbbd2e82558d304ee
Ahmed Alhassanat <a.hassanat89@gmail.com>
2026-08-03 00:23:02 +0200

    Remove spy_game function from Functions.py
    Removed the spy_game function that checks for the sequence 007.

--- udemy-python-bootcamp/Functions.py
+++ udemy-python-bootcamp/Functions.py
@@ -1,22 +1,4 @@
 
-
-'''
-SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order
-'''
-def spy_game(nums):
-    #007 order list
-    code = [0,0,7]
-    # position in the above code list
-    code_index = 0
-    for num in nums:
-        if num == code[code_index]:
-            code_index += 1
-        if code_index == len(code):
-            return True
-    return False
-
-**************************************************************************************************************************************************************************
-
 '''
 COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number
 '''
