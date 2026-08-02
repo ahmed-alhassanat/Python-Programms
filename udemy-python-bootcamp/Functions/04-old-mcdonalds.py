96de8b403747d36f8dba64fdc4c053e25e8c4076
Ahmed Alhassanat <a.hassanat89@gmail.com>
2026-08-03 00:15:19 +0200

    Implement old_macdonald function for string capitalization
    Added a function to capitalize the first and fourth letters of a name string.

--- udemy-python-bootcamp/Functions/04-old-mcdonalds.py
+++ udemy-python-bootcamp/Functions/04-old-mcdonalds.py
@@ -1,1 +1,15 @@
 
+
+'''
+OLD MACDONALD: Write a function that capitalizes the first and fourth letters of a  string
+'''
+
+def old_macdonald(name):
+    if len(name) > 3:
+        name_list = list(name)
+        name_list[0] = name_list[0].capitalize()
+        name_list[3] = name_list[3].capitalize()
+        name_new = ''.join(name_list)
+    else:
+        return 'Name is too short!'
+    return name_new
