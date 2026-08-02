270ddb75ffe80804d9e368ef29bc909bcea1e81a
Ahmed Alhassanat <a.hassanat89@gmail.com>
2026-08-03 00:10:47 +0200

    Remove guessing game and add even number printer
    Removed a simple guessing game implementation and added a function to print even numbers.

--- udemy-python-bootcamp/Functions.py
+++ udemy-python-bootcamp/Functions.py
@@ -1,34 +1,4 @@
-# a simple game to guess the index of 'O' in a shuffled list
-from random import shuffle
-# Function to shuffle the initial list
-def shuffle_list(mylist):
-    shuffle(mylist)
-    return mylist
 
-# Input Function to receive the player's guess
-def player_guess():
-    guess = ''
-    while guess not in ['0', '1', '2']:
-        guess = input('Pick an index number: 0, 1 or 2 ')
-        
-    return int(guess)
-
-# Function to check the player's guess
-def check_guess(mylist, guess):
-    if mylist[guess] == 'O':
-        print('Correct!')
-    else:
-        print('Wrong!')
-        print(mylist)
-
-# initial list
-mylist = ['', 'O', '']
-# shuffle list
-shuffled_list = shuffle_list(mylist)
-#user guess
-guess = player_guess()
-# check guess
-check_guess(shuffled_list, guess)
 ********************************************************************************************************************************************
 
 # Function that prints only even numbers #
